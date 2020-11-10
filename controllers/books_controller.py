765eb10558b36758fcd6e548d936b3dfc084596d
Bruce McClure <brucemcc90@gmail.com>
2020-11-11 10:55:07 +1100

    book post is working

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,25 +1,31 @@
-from models.Book import Book                  # This is the module that will communicate with the book table
-from main import db                           # Db connection
-from flask import Blueprint, request, jsonify # We need to be able to create a blueprint and retrieve and send back data
-from schemas.BookSchema import books_schema     # Importing the serialization module
+from models.Book import Book                              # This is the module that will communicate with the book table
+from main import db                                       # Db connection
+from flask import Blueprint, request, jsonify             # We need to be able to create a blueprint and retrieve and send back data
+from schemas.BookSchema import books_schema, book_schema  # Importing the serialization module
 books = Blueprint("books", __name__, url_prefix="/books") # Creating the blueprint and specifying the url_prefix
 
 
 # These are examples of raw sql crud. They will be replaced with a ORM soon
 
+#Return all books
 @books.route("/", methods=["GET"])
 def book_index():
-    #Return all books
-    books = Book.query.all()
-    return jsonify(books_schema.dump(books))
+    books = Book.query.all()                   # Using the Book model to fetch all the books with the query method
+    return jsonify(books_schema.dump(books))   # Return the data in the form of JSON
     
 
-# @books.route("/", methods=["POST"])
-# def book_create():
-#     #Create a new book
-#     sql = "INSERT INTO books (title) VALUES (%s);"
-#     cursor.execute(sql, (request.json["title"],))
-#     connection.commit()
+@books.route("/", methods=["POST"])
+def book_create():
+    #Create a new book
+    book_fieds = book_schema.load(request.json) # Loading the data that was sent in the post
+
+    new_book = Book()                           # Creating a new instance of book
+    new_book.title = book_fieds["title"]        # Update the title
+
+    db.session.add(new_book)                  # Add the book to the session
+    db.session.commit()                         # Commit the transaction
+
+    return jsonify(book_schema.dump(new_book))                            # Return the json format of the book
 
 #     sql = "SELECT * FROM books ORDER BY ID DESC LIMIT 1"
 #     cursor.execute(sql)

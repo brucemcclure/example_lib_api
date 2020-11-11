85dad92ebdb94ad12e27c5523133c259fa9d854f
Bruce McClure <brucemcc90@gmail.com>
2020-11-11 15:28:50 +1100

    removed database.py

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-from dotenv import load_dotenv                              # This is the package used to set env varibles
-load_dotenv()                                               # Load the env variables
-
-from marshmallow.exceptions import ValidationError
-
 from flask import Flask, jsonify                            # Importing the flask class
-app = Flask(__name__)                                       # Creating an app instnace from the flask class
-app.config.from_object("default_settings.app_config")       # Loads our configuration from defatult_settings.py
+from marshmallow.exceptions import ValidationError
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
-from database import init_db
-db = init_db(app)                                           # Db connection
-
-# Serialization and Deserialization
-from flask_marshmallow import Marshmallow                   # Importing the package 
-ma = Marshmallow(app)                                       # Making a 'ma' object out of the Marshmellow class with app as the argument. 
-
-from commands import db_commands                            # Import the db commands
-app.register_blueprint(db_commands)                         # Register the commands with app
-
-from controllers import registerable_controllers            # Importing the various controllers
-for controller in registerable_controllers:                 # Looping over the registerable_controllers and registering them
-    app.register_blueprint(controller)
+db = SQLAlchemy()
+ma = Marshmallow()
 
 
-@app.errorhandler(ValidationError)                          # Decorator for the ValidationError
-def handle_bad_request(error):                              # The handle bad request function inherits from the python error object
-    return (jsonify(error.messages), 400)                   # Return the  error message in jason with a status of 400
+# The factory pattern is where you create an object without exposing its creation logic. 
+# Basically the factory pattern uses a function to instantiate a new object and then returns that object from the function.
+
+def create_app():                                               # Flask convention for when you want to create an app in the factory pattern
+    from dotenv import load_dotenv                              # This is the package used to set env varibles
+    load_dotenv()                                               # Load the env variables
+
+
+    app = Flask(__name__)                                       # Creating an app instnace from the flask class
+    app.config.from_object("default_settings.app_config")       # Loads our configuration from defatult_settings.py
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    from commands import db_commands                            # Import the db commands
+    app.register_blueprint(db_commands)                         # Register the commands with app
+
+    from controllers import registerable_controllers            # Importing the various controllers
+    for controller in registerable_controllers:                 # Looping over the registerable_controllers and registering them
+        app.register_blueprint(controller)
+
+
+    @app.errorhandler(ValidationError)                          # Decorator for the ValidationError
+    def handle_bad_request(error):                              # The handle bad request function inherits from the python error object
+        return (jsonify(error.messages), 400)                   # Return the  error message in jason with a status of 400
+
+    return app

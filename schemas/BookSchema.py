7b9133b8c24159dd56c2277d3f1851bceab00647
Bruce McClure <brucemcc90@gmail.com>
2020-11-11 15:19:32 +1100

    validated length of input

--- schemas/BookSchema.py
+++ schemas/BookSchema.py
@@ -1,12 +1,14 @@
-from main import ma          # Importing the instnace of ma, which has app registered with it. 
-from models.Book import Book # Importing the model that handles the book database.
+from main import ma                          # Importing the instnace of ma, which has app registered with it. 
+from models.Book import Book                 # Importing the model that handles the book database.
+from marshmallow.validate import Length      # This is the validator module in marshmallow
+
 
 
 # Because we are using the auto schema marshmello will auto update the schema when we change the book model 
 class BookSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Book
-    title = ma.String(required=True)        # This is declaring a required data type on the title column
+    title = ma.String(required=True, validate=Length(min=1))        # This is declaring a required data type on the title column
 
 book_schema = BookSchema()                  # How you serilaize and deserialize one object.
 books_schema = BookSchema(many = True)      # How you serilaize and deserialize many objects.

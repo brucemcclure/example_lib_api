1972e0fbea8ff823edcb5f9604c4fafd72552f88
Bruce McClure <brucemcc90@gmail.com>
2020-11-11 14:42:57 +1100

    working config

--- default_settings.py
+++ default_settings.py
@@ -1,22 +1,22 @@
 import os  # This is the OS package which is used to retrieve environment variables
 
+# Parent class called config. All other classes inherit from it.
+class Config(object):                                   
+    #connect to postgres+using psycopg2://username:password@localhost:port/name_of_db 
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://app:{os.getenv('DB_PASSWORD')}@localhost:5432/library_api"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # If not needed then this should be disabled because it uses extra memory.
 
-#connect to postgres+using psycopg2://username:password@localhost:port/name_of_db 
-class Config(object):
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg2://app:{os.getenv('DB_PASSWORD')}@localhost:5432/library_api"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # If not needed then this should be disabled because it uses extra memory.
 
-
-class DevelopmentConfig(Config):
+class DevelopmentConfig(Config): # Inherits from config
     DEBUG = True
 
-class ProductionConfig(Config):
+class ProductionConfig(Config): # Inherits from config
     pass
 
-class TestingConfig(Config):
+class TestingConfig(Config): # Inherits from config
     TESTING = True
 
-environment = os.environ.get("FLASK_ENV")
+environment = os.environ.get("FLASK_ENV") # Retrieve the the flask env variable
 
 if environment == "production":
     app_config = ProductionConfig()

f9530dcb34924fec4995d3f3f5acfa78f6043bbe
Bruce McClure <brucemcc90@gmail.com>
2020-11-12 21:27:13 +1100

    registered the auth blueprint

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,11 @@
 # When you import a directory but you havent given a specific file name then it will import __init__.py by default
 
 from controllers.books_controller import books # Importing the books blueprint
+from controllers.auth_controller import auth   # Importing the auth blueprint
 
 registerable_controllers = [
-    books
+    books,
+    auth
 ]
 
 

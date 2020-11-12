e08f909f1b083a5472737161e21bbd6d6fc55910
Bruce McClure <brucemcc90@gmail.com>
2020-11-12 21:35:40 +1100

    user model

---
+++ models/User.py
@@ -0,0 +1,11 @@
+from main import db                                                 # Importing the db from main 
+
+class User(db.Model):                                               # This is the the User class that inherits from db.Model
+    __tablename__ = "users"                                         # This explicitally names the table 'users'
+
+    id = db.Column(db.Integer, primary_key=True)                    # The id is the primary key
+    email = db.Column(db.String(), nullable=False, unique=True)     # The emails is a string and mustbe unique
+    password = db.Column(db.String(), nullable=False)               # The password is a string and cannot be null
+
+    def __repr__(self):                                             # 
+        return f"<User {self.email}>"                               #

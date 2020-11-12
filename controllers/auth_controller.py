68432e962b3337120bd2a7636fe23c2a1304812d
Bruce McClure <brucemcc90@gmail.com>
2020-11-12 21:25:58 +1100

    auth controller comments

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,7 +1,7 @@
-from flask import Blueprint                             
+from flask import Blueprint                                # Importing the Bllueprint class from flask  
 
-auth = Blueprint('auth', __name__, url_prefix="/auth")
+auth = Blueprint('auth', __name__, url_prefix="/auth")     # Creating the auth blueprint into a varaibale called auth
 
-@auth.route("/register", methods=["POST"])
+@auth.route("/register", methods=["POST"])                 # Register route
 def auth_register():
     return "working"

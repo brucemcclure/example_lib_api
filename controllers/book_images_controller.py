6d004624d5d162b40ab90a4ae456383384f5fd8a
Bruce McClure <brucemcc90@gmail.com>
2020-11-16 13:41:07 +1100

    local file upload working

--- controllers/book_images_controller.py
+++ controllers/book_images_controller.py
@@ -7,12 +7,19 @@ from schemas.BookImageSchema import book_image_schema
 
 book_images = Blueprint("book_images", __name__, url_prefix="/books/<int:book_id>/image")  # The book id is the book the image is assiciated with
 
+# NB if flaask receives an image it puts it in a dictionary called request.files
+
 
 @book_images.route("/", methods=["POST"])
 @jwt_required
 @verify_user
 def book_image_create(book_id, user=None):
-    return "1"
+    if "image" in request.files:
+        image = request.files["image"]
+        image.save("uploaded_images/file_1.png")
+        return("", 200)
+
+    return abort(400, description="no image")
 
 
 @book_images.route("/<int:id>", methods=["GET"])
